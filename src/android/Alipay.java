9050fc6961425e55f67860ee3544ab2217d87031
王贺 <wanghe@propersoft.cn>
2016-08-16 16:22:43 +0800

    去掉支付宝插件安卓部分多余代码

--- src/android/Alipay.java
+++ src/android/Alipay.java
@@ -79,18 +79,6 @@ public class Alipay extends CordovaPlugin {
 				cbContext.sendPluginResult(result);
 				return ret;
 			}
-
-			if (!isSameSignature(payInfo, sign)) {
-				LOG.e(LOG_TAG, "pay_info sign failure.",
-						new IllegalStateException());
-				ret = false;
-				PluginResult result = new PluginResult(
-						PluginResult.Status.ERROR, "pay_info sign failure.");
-				result.setKeepCallback(true);
-				cbContext.sendPluginResult(result);
-				return ret;
-			}
-
 			Runnable payRunnable = new Runnable() {
 
 				@Override
@@ -149,41 +137,5 @@ public class Alipay extends CordovaPlugin {
 		return true;
 	}
 
-	/**
-	 * 判断签名是否一致
-	 * 
-	 * @param orign
-	 *            原始值
-	 * @param sign
-	 *            签名
-	 * @return 一致:true
-	 */
-	private boolean isSameSignature(String orign, String sign) {
-		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-				'A', 'B', 'C', 'D', 'E', 'F' };
-		try {
-			byte[] btInput = orign.getBytes("UTF-8");
-			// 获得MD5摘要算法的 MessageDigest 对象
-			MessageDigest mdInst = MessageDigest.getInstance("MD5");
-			// 使用指定的字节更新摘要
-			mdInst.update(btInput);
-			// 获得密文
-			byte[] md = mdInst.digest();
-			// 把密文转换成十六进制的字符串形式
-			int j = md.length;
-			char str[] = new char[j * 2];
-			int k = 0;
-			for (int i = 0; i < j; i++) {
-				byte byte0 = md[i];
-				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-				str[k++] = hexDigits[byte0 & 0xf];
-			}
-
-			return new String(str).equals(sign);
-		} catch (Exception e) {
-			LOG.e(LOG_TAG, e.getMessage(), e);
-			return false;
-		}
-	}
 
 }
